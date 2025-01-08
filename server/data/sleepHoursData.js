71923ddb14d74a901571a3becf761c04e1c5c044
NS <noyshlomo2004@gmail.com>
2025-01-08 22:42:21 +0200

    Add sleep hours  data file

---
+++ server/data/sleepHoursData.js
@@ -0,0 +1,39 @@
+const sleepHoursData = [
+  {
+    "userId": "3",
+    "sleepTime": "23:50",
+    "wakeUpTime": "22:50",
+    "date": "2025-01-08",
+    "sleepId": 1
+  },
+  {
+    "userId": "3",
+    "sleepTime": "22:48",
+    "wakeUpTime": "07:48",
+    "date": "2025-01-08",
+    "sleepId": null
+  },
+  {
+    "userId": "3",
+    "sleepTime": "23:02",
+    "wakeUpTime": "22:04",
+    "date": "2025-01-08",
+    "sleepId": null
+  },
+  {
+    "userId": "3",
+    "sleepTime": "12:10",
+    "wakeUpTime": "23:10",
+    "date": "2025-01-08",
+    "sleepId": null
+  },
+  {
+    "userId": "1",
+    "sleepTime": "23:46",
+    "wakeUpTime": "08:22",
+    "date": "2025-01-08",
+    "sleepId": null
+  }
+];
+
+export default sleepHoursData;
