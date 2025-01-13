a7c0e775613b669d41bb6431cf3074b7749767ab
Liel Maoz <lielmaoz1@gmail.com>
2025-01-14 01:05:21 +0200

    Update navigation to use /inspiration-boards route instead of /inspiration-dashboard

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -2,9 +2,7 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
 
-
 const Home = () => {
-
   const navigate = useNavigate();
 
   const handleSleepNavigation = () => {
@@ -13,8 +11,11 @@ const Home = () => {
   };
 
   const handleInspirationNavigation = () => {
+    /*
     const userId = '2'; // Temp user ID
     navigate(`/inspiration-dashboard/${userId}`);
+    */
+    navigate(`/inspiration-boards`);
   };
 
   return (
@@ -22,11 +23,22 @@ const Home = () => {
       <h1 className={styles.headline}>Welcome back, Candy</h1>
       <div className={styles.container}>
         <div>Your Goals</div>
-        <button type="button" className={styles.sleepButton} onClick={handleSleepNavigation}>
+        <button
+          type="button"
+          className={styles.sleepButton}
+          onClick={handleSleepNavigation}
+        >
           <h2>Sleep Tracking</h2>
-          <p>Update ypur sleep log, get your sleep analytics and change your goals</p>
+          <p>
+            Update ypur sleep log, get your sleep analytics and change your
+            goals
+          </p>
         </button>
-        <button type="button" className={styles.inspirationButton}  onClick={handleInspirationNavigation}>
+        <button
+          type="button"
+          className={styles.inspirationButton}
+          onClick={handleInspirationNavigation}
+        >
           <h2>Inspiration Dashboard</h2>
           <p>Explore wellness topics and discover influencers.</p>
         </button>
