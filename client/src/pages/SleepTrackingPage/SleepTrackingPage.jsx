31caf3d2fa9a3f1c7b05357af5027dd80441530d
NS <noyshlomo2004@gmail.com>
2025-01-19 22:28:35 +0200

    Fix sleep tracking page style to be responsive

--- client/src/pages/SleepTrackingPage/SleepTrackingPage.jsx
+++ client/src/pages/SleepTrackingPage/SleepTrackingPage.jsx
@@ -18,34 +18,34 @@ const SleepTrackingPage = () => {
     };
 
     return (
-<div>
-      <h1>Sleep Tracking</h1>
-      <SleepTrackingForm userId={id} />
+      <div>
+        <h1>Sleep Tracking</h1>
+        <SleepTrackingForm userId={id} />
 
-      <div className={styles['buttons-container']}>
-        <button
-          className={styles['sleep-history-button']}
-          aria-label="View Sleep History"
-          onClick={goToSleepAnalytics}
-        >
-          Sleep History
-        </button>
-        <button
-          className={styles['set-sleep-goals-button']}
-          aria-label="Set Sleep Goals"
-          onClick={() => console.log('Set Sleep Goals')}
-        >
-          Set Sleep Goals
-        </button>
-        <button
-          className={styles['sleep-tips-button']}
-          aria-label="View Tips for Sleep Hygiene"
-          onClick={handleSleepTipsButtonClick}
-        >
-          Tips for Sleep Hygiene
-        </button>
+        <div className={styles['buttons-container']}>
+          <button
+            className={styles['sleep-history-button']}
+            aria-label="View Sleep History"
+            onClick={goToSleepAnalytics}
+          >
+            Sleep History
+          </button>
+          <button
+            className={styles['set-sleep-goals-button']}
+            aria-label="Set Sleep Goals"
+            onClick={() => console.log('Set Sleep Goals')}
+          >
+            Set Sleep Goals
+          </button>
+          <button
+            className={styles['sleep-tips-button']}
+            aria-label="View Tips for Sleep Hygiene"
+            onClick={handleSleepTipsButtonClick}
+          >
+            Tips for Sleep Hygiene
+          </button>
+        </div>
       </div>
-    </div>
     );
 };
 
