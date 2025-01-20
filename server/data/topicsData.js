791e29b749b2574a165debbfdeaddb8e01297d90
NS <noyshlomo2004@gmail.com>
2025-01-20 15:06:30 +0200

    Add url format to topics data

--- server/data/topicsData.js
+++ server/data/topicsData.js
@@ -2,18 +2,22 @@ const topics = [
     {
         id: 1,
         category: 'Nutrition',
+        url: 'nutrition',
     },
     {
         id: 2,
         category: 'Mental Health',
+        url: 'mental-health',
     },
     {
         id: 3,
         category: 'Body Image & Self-Esteem',
+        url: 'body-image-and-self-esteem',
     },
     {
         id: 4,
         category: 'Sexuality & Health',
+        url: 'sexuality-and-health',
     },
 ]
 
